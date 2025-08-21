9f41620a55d1484ddb820106300fb50999163d05
Ken Tsukamoto <tsukaken0622@gmail.com>
2025-08-22 00:20:22 +0900

    ✨ Todoの作成

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function InputForm() {
+  return (
+    <div>
+      <form>
+        <input />
+        <button>
+          
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default InputForm
---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div>
+      <header>
+        <h1>ようこそ TODO LISTへ</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Title
